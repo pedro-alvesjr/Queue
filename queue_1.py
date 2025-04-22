67e516c39b864221039d57e44767c1608233f0c7
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-21 21:14:50 -0300

    Update queue_1.py
    Added enqueue(value) method.

--- queue_1.py
+++ queue_1.py
@@ -16,6 +16,16 @@ class Queue:
             print(temp.value)
             temp = temp.next
 
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
 my_queue = Queue(1)
 
 my_queue.print_queue()
