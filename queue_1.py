a216e6366a806b542b45ca548deef72e0aa7d63a
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-04-22 21:30:46 -0300

    Update queue_1.py
    Added dequeue() method.

--- queue_1.py
+++ queue_1.py
@@ -26,6 +26,19 @@ class Queue:
             self.last = new_node
         self.length += 1
 
+    def dequeue(self):
+        if self.first is None:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
 my_queue = Queue(1)
 
 my_queue.print_queue()
